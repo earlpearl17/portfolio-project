07bc7967247bb99885b4b6ea6ae2217ca0512ed3
Earl Pearl <earlpearl17@gmail.com>
2019-05-27 13:36:32 -0400

    Django 2 Portfolio project complete

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,12 @@ class Blog(models.Model):
     #body = models.CharField(max_length=400)
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title + " " + str(self.pub_date)
+
+    def summary(self):
+        return self.body[:100] + "..."
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Blog
+
+def allblogs(request):
+    blogs = Blog.objects
+    # return objects in descending order
+    #blogs = Blog.objects.order_by('-pub_date')
+    #filter(client=client_id).order_by('-check_in')
+    return render(request, 'blog/allblogs.html', {'blogs':blogs})
+
+def detail(request, blog_id):
+    detailblog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog':detailblog})
