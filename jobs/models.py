9311915de3834d6836b2f65867bac935c5d76408
Earl Pearl <earlpearl17@gmail.com>
2020-02-27 10:22:00 -0500

    Added fields to Job model & corresponding template (home.html)

--- jobs/models.py
+++ jobs/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
 class Job(models.Model):
+    #title = models.CharField(max_length=50, default='ENTER TITLE')
+    title = models.CharField(max_length=50)
+    #url = models.TextField(default='ENTER URL')
+    #url = models.URLField(max_length=200, default='ENTER URL')
+    url = models.URLField(max_length=200)
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        if len(self.summary) > 50:
+            return self.summary[:50] + "..."
+        else:
+            return self.summary
